0feacbb4e769e592033e127c101593e5bc6dfa09
Diogo Santos <diogo.magalhaescalvo@gmail.com>
2023-06-21 17:15:56 +0100

    added project

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -15,7 +15,7 @@ const Ball = props => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={3.75} rotationIntensity={3} floatIntensity={3}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 0, 0.05]} intensity={1} />
 
